0d614bdc6e92b6107fe6e86b19bc3415937555a6
Tran Duc <48123564+ducxda92@users.noreply.github.com>
2021-10-15 14:42:49 +0700

    Update centos8ipauth.sh

--- centos8ipauth.sh
+++ centos8ipauth.sh
@@ -54,8 +54,12 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-$(awk -F "/" '{print "auth iponly\n" \
-"allow * 58.186.61.26 * *"\n" \
+auth iponly strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+allow * 58.186.61.26
+$(awk -F "/" '{print "auth iponly strong\n" \
+"allow * 58.186.61.26"\n" \
+"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
