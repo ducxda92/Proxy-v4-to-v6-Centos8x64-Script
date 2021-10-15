40d4262f85ff62bf257cc93bffe52ca39cce87a3
Tran Duc <48123564+ducxda92@users.noreply.github.com>
2021-10-15 13:42:46 +0700

    Update scriptcentos8.sh

--- scriptcentos8.sh
+++ scriptcentos8.sh
@@ -54,11 +54,10 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth iponly strong
+auth strong
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth iponly strong\n" \
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"allow * 58.186.61.26"\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
