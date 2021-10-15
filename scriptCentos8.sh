0bfd0d9e553e3dcaa7c7c31147117bd6e8ddeee3
Tran Duc <48123564+ducxda92@users.noreply.github.com>
2021-10-15 12:58:36 +0700

    Update scriptCentos8.sh

--- scriptCentos8.sh
+++ scriptCentos8.sh
@@ -54,11 +54,10 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth iponly strong
+auth strong
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth iponly strong\n" \
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"allow * 192.168.0.0/16" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
